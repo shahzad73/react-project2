e5cfb1e64c7fad02b308b39c11d7c55b3fb3a6c8
Shahzad Aslam <sa@digishares.io>
2023-12-03 20:45:44 +0500

    some changes

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -2,9 +2,11 @@ import '../App.css';
 import React from 'react';
 import { Card } from 'semantic-ui-react';
 import { Button } from 'semantic-ui-react';
+import { BrowserExtensionSigningManager } from '@polymeshassociation/browser-extension-signing-manager';
+import { Polymesh } from '@polymeshassociation/polymesh-sdk';
 
 
-const Main = () => {
+const Main =  () => {
 
   const items = [
     {
@@ -20,6 +22,33 @@ const Main = () => {
       meta: 'ROI: 34%',
     },
   ]
+
+    async () => {
+        // setup. This call will prompt the user if they haven't authorized the dApp before
+        const signingManager = await BrowserExtensionSigningManager.create({
+          appName: 'my-dApp',
+          //extensionName: 'polywallet', // this is optional, defaults to 'polywallet'
+        });
+
+        const polymesh = await Polymesh.connect({
+          nodeUrl: "wss://testnet-rpc.polymesh.live",
+          signingManager,
+        });
+
+        // callback is called whenever the extension Accounts change
+        signingManager.onAccountChange(newAccounts => {
+          // change SDK's signing account, reload the page, do whatever
+          alert( newAccounts )
+        });
+
+        // callback is called whenever the extension's selected network changes
+        signingManager.onNetworkChange(newNetwork => {
+          // act accordingly
+          alert( newNetwork )
+        });
+
+      }
+      
 
   return (
     <div>
